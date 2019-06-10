e3de7b8e21fe5174f1ad09b96febcfb41d19a95d
aminemech <48395151+aminemech@users.noreply.github.com>
2019-06-10 22:55:18 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`ana mchghol kanl3b sir 9wd `,"http://twitch.tv/Dream")
+client.user.setGame(`L7awaayyaaa #Gang `,"http://twitch.tv/Dream")
 client.user.setStatus("dnd")
 });
 
